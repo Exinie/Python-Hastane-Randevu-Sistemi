a62f4c10931411f6cb88b9896fbae16cf3acd6dd
Exinie <75310852+Exinie@users.noreply.github.com>
2025-05-12 22:31:06 +0300

    Doktor oturum işlemi fonksiyonu yazıldı, doktor şifre doğrulama yazıldı, session sistemi oluşturuldu.

--- oturum_islemleri.py
+++ oturum_islemleri.py
@@ -2,7 +2,31 @@ import tkinter as tk
 from tkinter import messagebox
 import sqlite3
 
-from main import *
+
+# Oturum array listesi
+oturum = []
+
+# Kullanıcı oturumu oluşturma fonksiyonu
+def oturum_oluştur(giris_yapilan_oturum_bilgileri=[]):
+    oturum.append(giris_yapilan_oturum_bilgileri)
+    print(f"[+] Oturum oluşturuldu: {oturum}")
+
+def doktor_sifre_dogrula(tc_no, sifre):
+
+    conn = sqlite3.connect('veritabani.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        SELECT tc_no FROM DOKTORLAR WHERE tc_no = ? AND sifre = ? LIMIT 1;
+    ''', (tc_no, sifre))
+
+    sonuc = cursor.fetchone()
+    conn.close()
+
+    if sonuc:
+        return True
+    else:
+        return False
 
 def hasta_tc_kayitli_mi(tc_no):
 
@@ -12,7 +36,7 @@ def hasta_tc_kayitli_mi(tc_no):
     cursor.execute('''
         SELECT tc_no FROM HASTALAR WHERE tc_no = ? LIMIT 1;
     ''', (tc_no,))
-
+    
     sonuc = cursor.fetchone()
     conn.close()
 
@@ -44,8 +68,35 @@ def hasta_kayit_ol(tc_no, sifre, ad, soyad):
     else:
         messagebox.showwarning("Uyarı", "Lütfen tüm alanları doldurunuz.")
         return False
+
+def doktor_oturum_ac(tc_no, sifre):
+   
+    if tc_no.strip() and sifre.strip():
+
+        if len(tc_no) < 11:
+            messagebox.showwarning("Uyarı", "Geçersiz TC kimlik numarası.")
+        elif doktor_sifre_dogrula(tc_no, sifre):
+            messagebox.showinfo("Bilgi", "Hoş geldiniz!")
+
+            conn = sqlite3.connect('veritabani.db')
+            cursor = conn.cursor()
+
+            cursor.execute('''
+                SELECT * FROM DOKTORLAR WHERE tc_no = ?;
+            ''', (tc_no,))
+
+            doktor_giris_yapilan_kullanici_bilgileri = cursor.fetchone()
+            conn.close()
+
+            oturum_oluştur(doktor_giris_yapilan_kullanici_bilgileri)
+            return True
+        else:
+            messagebox.showwarning("Uyarı", "TC kimlik numarası veya şifre yanlış!")
+
+    else:
+        messagebox.showwarning("Uyarı", "Lütfen tüm alanları doldurunuz.")
+        return False
     
-def oturum_ac(kullaniciadi, sifre):
+def hasta_oturum_ac(tc_no, sifre):
     return False
 
-    
