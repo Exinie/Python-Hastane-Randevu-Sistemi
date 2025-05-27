b246ae32256c93dbff6086dcb89a802a5623717a
MrToothlesss <206591784+MrToothlesss@users.noreply.github.com>
2025-05-27 23:46:47 +0300

    Randecu Listeleme, Silme işlemleri Fonksiyonları Eklendi.

--- doktor_islemleri.py
+++ doktor_islemleri.py
@@ -1,2 +1,44 @@
 import sqlite3
 
+class DoktorIslemleri:
+    def __init__(self):
+        self.veritabani_dosyasi = "veritabani.db"
+    def randevulari_listele(self, doktor_id):
+        try:
+            conn = sqlite3.connect(self.veritabani_dosyasi)
+            cursor = conn.cursor()
+            cursor.execute("SELECT randevu_id, hasta_id, tarih, saat, sikayet, durum FROM RANDEVULAR WHERE doktor_id = ?", (doktor_id,))
+            sonuc = cursor.fetchall()
+            return sonuc
+        except:
+            return "veritabani_hatasi"
+        finally:
+            conn.close()
+
+    def randevu_sil(self, randevu_id):
+        try:
+            conn = sqlite3.connect(self.veritabani_dosyasi)
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM RANDEVULAR WHERE randevu_id=?", (randevu_id,))
+            conn.commit()
+            if cursor.rowcount == 0:
+                return "randevu_bulunamadi"
+            return "basarili"
+        except:
+            return "veritabani_hatasi"
+        finally:
+            conn.close()
+
+    def randevu_durum_guncelle(self, randevu_id, yeni_durum):
+        try:
+            conn = sqlite3.connect(self.veritabani_dosyasi)
+            cursor = conn.cursor()
+            cursor.execute("UPDATE RANDEVULAR SET durum=? WHERE randevu_id=?", (yeni_durum, randevu_id))
+            conn.commit()
+            if cursor.rowcount == 0:
+                return "randevu_bulunamadi"
+            return "basarili"
+        except:
+            return "veritabani_hatasi"
+        finally:
+            conn.close()
